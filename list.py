20777e6dd0f3a58e13367ff24e53930aa0b2ad82
Gogs <gogs@fake.local>
2016-11-04 12:51:02 -0600

    fdfdf

--- list.py
+++ list.py
@@ -40,3 +40,56 @@ print animals.index("bat")
 # insert at a specific index
 animals.insert(1, "dog")
 print animals
+
+
+beatles = ["john","paul","george","ringo","stuart"]
+beatles.remove("stuart")
+print beatles
+
+
+d = {'key1' : 1, 'key2' : 2, 'key3' : 3}
+
+print d["key1"]
+
+
+d["key4"] = 4
+
+print d["key4"]
+
+print d
+
+my_dict = {
+    "fish": ["c", "a", "r", "p"],
+    "cash": -4483,
+    "luck": "good"
+}
+print my_dict["fish"][0]
+
+
+
+
+once  = {'a': 1, 'b': 2}
+twice = {'a': 2, 'b': 4}
+for key in once: # for on a hash/dictionary
+    print "Once: %s" % once[key]
+    print "Twice: %s" % twice[key]
+
+
+
+
+animal_sounds = {
+    "cat": ["meow", "purr"],
+    "dog": ["woof", "bark"],
+    "fox": [],
+}
+print animal_sounds["cat"]
+print animal_sounds
+
+
+students = ["lloyd", "alice", "tyler"]
+
+for student in students:
+    print student
+
+
+    
